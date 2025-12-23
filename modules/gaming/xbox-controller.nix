99a5b8ad232658b37c2cc5b8fb2eae932f86f258
Kyle Bartz <mail@kyle.fail>
2025-12-22 17:37:58 -0800

    Pinning xone to 0.4.8

--- modules/gaming/xbox-controller.nix
+++ modules/gaming/xbox-controller.nix
@@ -1,5 +1,30 @@
 {
   flake.modules.nixos.gaming = {
-    hardware.xone.enable = true;
+    config,
+    pkgs,
+    lib,
+    ...
+  }: {
+    # defining the following instead of using hardware.xone.enable in order to pin versions
+    # hardware.xone.enable = true;
+
+    boot = {
+      blacklistedKernelModules = [
+        "xpad"
+        "mt76x2u"
+      ];
+      extraModulePackages = with config.boot.kernelPackages; [
+        # Pinning to version 0.4.8 as 0.5.0 seems to not work with my dongle
+        (xone.overrideAttrs (prev: {
+          version = "0.4.8";
+          src = prev.src.override {
+            hash = "sha256-EXJBqzO4e2SJGrPvB0VYzIQf09uo5OfNdBQw5UqskYg=";
+          };
+        }))
+      ];
+    };
+
+    hardware.firmware = [pkgs.xow_dongle-firmware];
+    hardware.xpad-noone.enable = lib.mkDefault true;
   };
 }
