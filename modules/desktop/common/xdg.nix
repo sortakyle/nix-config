3b597bd18219ccdec564f75757a6383b678172ec
Kyle Bartz <mail@kyle.fail>
2025-12-27 12:27:45 -0800

    Moved Desktop dir to .desktop

--- modules/desktop/common/xdg.nix
+++ modules/desktop/common/xdg.nix
@@ -8,13 +8,17 @@
     ];
   };
 
-  flake.modules.homeManager.desktop = {
+  flake.modules.homeManager.desktop = {config, ...}: {
     # Manages file associations with default programs
     xdg.mimeApps.enable = true;
 
     # Enable the default user directories within home
     # Music, Videos, Desktop, Public, Pictures, Downloads, Documents, Templates
     xdg.userDirs.enable = true;
+
+    # Hiding the Desktop directory since I prefer to not use it
+    # alternatively can set it to NULL so XDG_DESKTOP_DIR is unset
+    xdg.userDirs.desktop = "${config.home.homeDirectory}/.desktop";
 
     # Hide the following desktop entries. Never want to launch these from a launcher
     xdg.desktopEntries = {
