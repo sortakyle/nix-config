0b36a68ce74dca6eafa63370a7b222fb68573768
Kyle Bartz <mail@kyle.fail>
2025-12-27 13:08:23 -0800

    Added firefox mime apps

--- modules/browser/firefox.nix
+++ modules/browser/firefox.nix
@@ -51,11 +51,25 @@ in {
       BROWSER = lib.mkDefault "firefox";
     };
 
-    xdg.mimeApps.defaultApplications = {
-      "text/html" = ["firefox.desktop"];
-      "text/xml" = ["firefox.desktop"];
-      "x-scheme-handler/http" = ["firefox.desktop"];
-      "x-scheme-handler/https" = ["firefox.desktop"];
+    xdg.mimeApps.defaultApplications = let
+      browser = ["firefox.desktop"];
+    in {
+      "text/html" = browser;
+      "text/xml" = browser;
+      "application/xml" = browser;
+      "application/xhtml+xml" = browser;
+      "application/xhtml_xml" = browser;
+      "application/rdf+xml" = browser;
+      "application/rss+xml" = browser;
+      "application/x-extension-htm" = browser;
+      "application/x-extension-html" = browser;
+      "application/x-extension-shtml" = browser;
+      "application/x-extension-xht" = browser;
+      "application/x-extension-xhtml" = browser;
+      "x-scheme-handler/about" = browser; # open `about:` url with `browser`
+      "x-scheme-handler/ftp" = browser; # open `ftp:` url with `browser`
+      "x-scheme-handler/http" = browser;
+      "x-scheme-handler/https" = browser;
     };
 
     stylix.targets.firefox = {
