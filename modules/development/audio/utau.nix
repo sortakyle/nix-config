40f1c43661b808690f8639b90aaa6623b79392b5
Kyle Bartz <mail@kyle.fail>
2026-01-05 21:35:51 -0800

    Upgraded openutau to 0.1.567

--- modules/development/audio/utau.nix
+++ modules/development/audio/utau.nix
@@ -1,13 +1,17 @@
 {
   flake.modules.homeManager.development = {pkgs, ...}: {
-    home.packages = with pkgs; [
-      openutau
+    home.packages = [
+      (pkgs.openutau.overrideAttrs (prev: {
+        version = "0.1.567";
+        src = prev.src.override {
+          hash = "sha256-tjW1xmt409AlEmw/N1RG46oigP4mWAoTecQGV/hwMo4=";
+        };
+      }))
     ];
   };
 
   flake.modules.nixos.development = {
     preserve.users.directories = [
-      ".config/OpenUtau"
       ".local/share/OpenUtau"
     ];
   };
