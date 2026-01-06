54397209aeb8d738d4bdfc8c403b2788c7efb7e1
Kyle Bartz <mail@kyle.fail>
2026-01-05 19:58:46 -0800

    Added derivations

--- modules/development/audio/utau/singers/nana.nix
+++ modules/development/audio/utau/singers/nana.nix
@@ -1,15 +1,39 @@
 {
   flake.modules.homeManager.development = {pkgs, ...}: let
-    files = pkgs.fetchzip {
-      url = "https://nanahira.jp/haruka_nana/data/renzokuon_2023.zip";
-      hash = "";
+    name = "openutau-singer-haruka-nana-v2023";
+
+    character = (pkgs.formats.yaml {}).generate "${name}-character.yaml" {
+      name = "春歌ナナ連続音_2023ver";
+      localized_names = {
+        en-US = "Haruka Nana (2023 version)";
+      };
+      singer_type = "utau";
+      text_file_encoding = "shift_jis";
+      image = "image.bmp";
+      author = "ななひら";
+      voice = "ななひら";
+      version = "2023";
+      web = "https://nanahira.jp/haruka_nana/index.html";
+      default_phonemizer = "OpenUtau.Plugin.Builtin.JapanesePresampPhonemizer";
     };
   in {
-    # 1. fetchzip
-    # 2. create character.yaml
-    # 3. bundle into single drv
-    # 4. link source
+    xdg.dataFile."OpenUtau/Singers/春歌ナナ連続音_2023ver".source = pkgs.stdenvNoCC.mkDerivation {
+      inherit name;
 
-    xdg.dataFile."OpenUtau/SingersTest/HarukaNana_2023ver".source = files;
+      src = pkgs.fetchzip {
+        url = "https://nanahira.jp/haruka_nana/data/renzokuon_2023.zip";
+        hash = "sha256-2pM0O0G6lX/0cHL5TchMSNe2Pgc1kpsqjg+AQxf50Q4=";
+      };
+
+      installPhase = ''
+        runHook preInstall
+
+        mkdir -p $out
+        cp -R $src/* $out/
+        cp ${character} $out/character.yaml
+
+        runHook postInstall
+      '';
+    };
   };
 }
